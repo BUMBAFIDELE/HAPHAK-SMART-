e6fddb1362398a253c2b37b202b5b743a49648d0
BUMBAFIDELE <142897347+BUMBAFIDELE@users.noreply.github.com>
2026-06-10 21:00:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "HAPHAK AI is running!"
+
+@app.route("/webhook", methods=["GET"])
+def verify():
+    verify_token = "haphak2026"
+
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == verify_token:
+        return challenge, 200
+
+    return "Verification failed", 403
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
