b271eb3346feeb3598727581972549d2b53192af
BUMBAFIDELE <142897347+BUMBAFIDELE@users.noreply.github.com>
2026-06-10 22:49:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,30 @@
 from flask import Flask, request
-import requests
 import os
-import google.generativeai as genai
 
 app = Flask(__name__)
-
-# =====================
-# ENV VARIABLES
-# =====================
-VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
-WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
-PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-# =====================
-# GEMINI SETUP
-# =====================
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-2.5-flash")
-
 
 @app.route("/")
 def home():
     return "HAPHAK AI is running!"
 
 
-# =====================
-# WEBHOOK VERIFY (META)
-# =====================
 @app.route("/webhook", methods=["GET"])
 def verify():
+    verify_token = os.getenv("VERIFY_TOKEN")
+
     mode = request.args.get("hub.mode")
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
 
-    if mode == "subscribe" and token == VERIFY_TOKEN:
+    if mode == "subscribe" and token == verify_token:
         return challenge, 200
 
     return "Verification failed", 403
 
 
-# =====================
-# RECEIVE WHATSAPP MESSAGE
-# =====================
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.get_json()
-print("DATA RECEIVED:", data)
-    try:
-        message = data["entry"][0]["changes"][0]["value"]["messages"][0]
-        text = message["text"]["body"]
-        sender = message["from"]
-
-        # ===== GEMINI RESPONSE =====
-        response = model.generate_content(text)
-        reply = response.text
-
-        # ===== SEND BACK TO WHATSAPP =====
-        url = f"https://graph.facebook.com/v19.0/{PHONE_NUMBER_ID}/messages"
-
-        headers = {
-            "Authorization": f"Bearer {WHATSAPP_TOKEN}",
-            "Content-Type": "application/json"
-        }
-
-        payload = {
-            "messaging_product": "whatsapp",
-            "to": sender,
-            "text": {"body": reply}
-        }
-
-        requests.post(url, headers=headers, json=payload)
-
-    except Exception as e:
-        print("Error:", e)
-
+    print("MESSAGE RECEIVED:", request.get_json())
     return "OK", 200
 
 
