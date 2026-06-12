df3aa152bb4ad9e515f4bab471f2adc4b25d16b6
BUMBAFIDELE <142897347+BUMBAFIDELE@users.noreply.github.com>
2026-06-12 09:36:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ print("=================================")
 # =========================
 try:
     genai.configure(api_key=GEMINI_API_KEY)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.0-flash")
     print("Gemini initialized successfully")
 except Exception as e:
     print("GEMINI INIT ERROR:", str(e))
